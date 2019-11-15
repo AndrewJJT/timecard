d7b4a9160c8aa15ffac2568421fdb643abf3e081
AndrewJJT <jjtan9886@gmail.com>
2019-11-15 14:52:09 -0500

    initial commit

---
+++ src/main/java/com/skillstrom/data/ConnectionFactory.java
@@ -0,0 +1,9 @@
+package com.skillstrom.data;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+//		public static Connection newConnection() {
+//			
+//		}
+}
---
+++ src/main/java/com/skillstrom/data/TimeSheetDAO.java
@@ -0,0 +1,10 @@
+package com.skillstrom.data;
+
+public class TimeSheetDAO {
+//	public User findbyUsername (String username) {}
+//	public List<TimeSheet> findTimesheetsByUser (int id){}
+//	public Timesheet findTImesheetById (int id) {}
+//	
+//	public TImesheet save(Timesheet t) {}
+	
+}
